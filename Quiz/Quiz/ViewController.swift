96cb4aa10b979f42298aa93f76eaf02f0e3ffb3f
HannaJeon <jks7psj@gmail.com>
2017-12-31 01:48:46 +0900

    Daily Study 1일

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by HannaJeon on 2017. 12. 30..
+//  Copyright © 2017년 HannaJeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?",
+                               "What is 7+7?",
+                               "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBAction func showNextQuestion(_ sender: Any) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: Any) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+}
+
---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by 전한나 on 2017. 12. 31..
+//  Copyright © 2017년 Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
