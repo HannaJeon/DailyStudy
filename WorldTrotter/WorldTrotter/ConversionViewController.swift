77cc1086048985e339335b959382ce9435caaec5
HannaJeon <jks7psj@gmail.com>
2018-01-01 00:49:14 +0900

    NumberFormatter(숫자 포맷 지정자) 사용해봄.. 소수점 자르기 꿀팁

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -13,11 +13,41 @@ class ConversionViewController: UIViewController {
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
-    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
-        if let text = sender.text, !text.isEmpty {
-            celsiusLabel.text = text
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: value))
         } else {
             celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        if let text = sender.text,
+            let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
         }
     }
     
