91bbc6d957c6df443ac94867d5248702879779b9
HannaJeon <jks7psj@gmail.com>
2018-01-01 23:31:07 +0900

    디렉토리 위치 변경

---
+++ 아론힐리가스의_iOS프로그래밍/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by HannaJeon on 2017. 12. 31..
+//  Copyright © 2017년 Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let textFieldSeparator = textField.text?.range(of: ".")
+        let replacementSeparator = string.range(of: ".")
+        let alphabets = CharacterSet.letters
+        let range = string.rangeOfCharacter(from: alphabets)
+        
+        if range != nil {
+            return false
+        }
+        
+        if textFieldSeparator != nil && replacementSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        if let text = sender.text,
+            let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+}
+
